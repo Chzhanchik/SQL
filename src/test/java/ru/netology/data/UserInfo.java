ecce62211a437318edfcbd4a6b7ba49e0759eb3b
chzhanchik <chzhanchik@gmail.com>
2022-01-08 17:36:11 +0300

    origin

---
+++ src/test/java/ru/netology/data/UserInfo.java
@@ -0,0 +1,15 @@
+
+package ru.netology.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserInfo {
+    String login;
+    String password;
+}
